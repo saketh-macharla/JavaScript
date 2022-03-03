b9f0aaf3a4f96d52bbd7ee00c10b6a97c9210b84
Saketh Macharla <saketh.macharla@zemosolabs.com>
2022-03-03 17:36:38 +0530

    Restaurant App

---
+++ Restuarant/scripts/menu.js
@@ -0,0 +1,44 @@
+menuItems= [
+    {
+        "id": 1,
+        "name": 'Crusty Garlic Focaccia with Melted Cheese',
+        "price": 105.00,
+        "category": 'entree',
+    },
+    {
+        "id": 2,
+        "name": "French Fries",
+        "price": 105.00,
+        "category": 'entree',
+    },
+    {
+        "id": 3,
+        "name": "Home Country Fries with Hearbs & Chilli Flakes",
+        "price": 105,
+        "category": 'entree',
+    },
+    {
+        "id": 4,
+        "name": "Chicken Biryani",
+        "price": 180,
+        "category": 'main',
+    },
+    {
+        "id": 5,
+        "name": "Mutton Biryani",
+        "price": 200,
+        "category": 'main',
+    },
+    {
+        "id": 6,
+        "name": "Gulab Jamun",
+        "price": 20,
+        "category": 'deserts',
+    },
+    {
+        "id": 7,
+        "name": "Cheese Cake",
+        "price": 150,
+        "category": 'deserts',
+    },
+]
